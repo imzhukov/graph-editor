e53a1523c5f8c7f20892b67cee6f7165acaeacee
Dmitry.Dr <didro.dr@yandex.ru>
2020-05-16 12:03:32 +0300

    Comment EventHandlersManager

--- api/src/main/java/de/tesis/dynaware/grapheditor/utils/EventHandlersManager.java
+++ api/src/main/java/de/tesis/dynaware/grapheditor/utils/EventHandlersManager.java
@@ -8,6 +8,11 @@ import javafx.scene.layout.Region;
 
 import java.util.HashMap;
 
+/**
+ * Improve management of event handlers.
+ * Main perspective is using storage for event handlers.
+ * Give opportunity to disable handlers temporarily
+ * */
 public class EventHandlersManager {
 
     private Node node;
@@ -18,11 +23,19 @@ public class EventHandlersManager {
         eventHandlersMap = new HashMap<>();
     }
 
+    /**
+     * Provide addEventHandler() and save a handler to storage
+     * */
     public void addAndSaveEventHandler(EventType type, EventHandler<? extends Event> eventHandler){
         node.addEventHandler(type, eventHandler);
         eventHandlersMap.put(eventHandler, type);
     }
 
+    /**
+     * Provide removeEventHandler() and can remove a handler from storage
+     *
+     * @param removeFromStorage - if true - remove handler from storage
+     * */
     public void removeEventHandler(EventType type, EventHandler<? extends Event> eventHandler, boolean removeFromStorage){
         node.removeEventHandler(type, eventHandler);
         if(removeFromStorage){
@@ -30,12 +43,19 @@ public class EventHandlersManager {
         }
     }
 
+    /**
+     * Provide removeEventHandler() for all handlers, but they stay in storage.
+     * If you want remove handlers from storage use following {@link #clearStorage}
+     * */
     public void removeAllEventHandlersFromNode(){
         eventHandlersMap.entrySet().forEach(entrySet -> {
             node.removeEventHandler(entrySet.getValue(), entrySet.getKey());
         });
     }
 
+    /**
+     * Restore all event handlers from storage
+     * */
     public void restoreEventHandlers(){
         eventHandlersMap.entrySet().forEach(entrySet -> {
             node.addEventHandler(entrySet.getValue(), entrySet.getKey());
